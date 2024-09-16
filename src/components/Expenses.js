c480e948a90e205c428a0b30e6ef224098c9f542
e2205680 <e2205680@vamk.fi>
2024-09-16 14:21:38 +0300

    TEHTÄVÄ 6

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,9 +1,10 @@
 import ExpenseItem from './ExpenseItem';
+import Card from './Card';
 import './Expenses.css';
 
 function Expenses(props) {
     return (      
-        <div className="expenses">
+        <Card className="expenses">
            
             <ExpenseItem
                 title={props.items[0].title}
@@ -26,7 +27,7 @@ function Expenses(props) {
             date={props.items[3].date}
             />
                         
-        </div>
+        </Card>
     );
 }
 
