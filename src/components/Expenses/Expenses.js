919621acedbbfc67be4b0b9e114658c7e3d01743
e2205680 <e2205680@vamk.fi>
2024-10-07 13:34:04 +0300

    TEHTÄVÄ 16

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -17,14 +17,17 @@ const Expenses = (props) => {
 const filteredExpenses = props.items.filter(expense => {
   return expense.date.getFullYear().toString() === filteredYear;
 });
-
+    /* Versio 1
     return (      
         <Card className="expenses">
             <ExpensesFilter 
             selected={filteredYear} 
             onChangeFilter={filterChangeHandler}
             />
-            {filteredExpenses.map((expense) => (
+            {filteredExpenses.length === 0 ? ( // versio 2 line 1: {filteredExpenses.lenght === 0 && <p>No expenses found.</p>}
+              <p>No expenses found.</p>        // versio 2 line 2:{filteredExpenses.length > 0 &&
+            ) : (                              
+            filteredExpenses.map((expense) => (
             //{props.items.map((expense) => (
               <ExpenseItem
                 key={expense.id}
@@ -32,8 +35,33 @@ const filteredExpenses = props.items.filter(expense => {
                 amount={expense.amount}
                 date={expense.date}
               />
-            ))}                      
+            ))
+            )}                      
         </Card>
+    ); */
+
+    // Versio 3
+    let expensesContent = <p>No expenses found.</p>;
+
+    if (filteredExpenses.length > 0) {
+      expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />  
+      ));
+    }
+
+    return (
+      <Card className="expenses">
+      <ExpensesFilter 
+      selected={filteredYear} 
+      onChangeFilter={filterChangeHandler}
+      />
+      {expensesContent}
+      </Card>
     );
 }
 
